24a107f1d00d7d19f927df5165876514d70ca94e
GuilhermeMendesLamounier <94761972+GuilhermeMendesLamounier@users.noreply.github.com>
2021-12-02 17:30:30 -0300

    add jump to prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,11 +6,21 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
+    public Vector2 friction = new Vector2(-.1f, 0);
+
     public Vector2 velocity;
 
     public float speed;
 
+    public float forceJump = 2;
+
     public void Update()
+    {
+        handleJump();
+        HandleMoviniment();
+    }
+
+    private void HandleMoviniment()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
@@ -23,6 +33,23 @@ public class Player : MonoBehaviour
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
         }
 
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity += friction;
+        }
 
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+
+    }
+
+    private void handleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * forceJump;
+        }
     }
 }
