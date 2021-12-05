b3c74e707e1dec099a18528e2a2d690ec61782dd
GuilhermeMendesLamounier <94761972+GuilhermeMendesLamounier@users.noreply.github.com>
2021-12-05 17:34:50 -0300

    atividade modulo 14

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 friction = new Vector2(-.1f, 0);
-
-    public Vector2 velocity;
-
+    [Header("Speed setup")]
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
     public float speedRun;
-
     public float forceJump = 2;
+    public float forcedown = 2;
+    
+
+
+    [Header("Animation setup pulo")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.7f;
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
+
+    [Header("Animation setup impacy")]
+    public float impactScaleY = 0.7f;
+    public float impactScaleX = 1.5f;
+    public float durationanimaation = 3f;
+    public Ease easa = Ease.OutBack;
 
     private float _currentSpeed;
 
@@ -21,12 +35,11 @@ public class Player : MonoBehaviour
     {
         handleJump();
         HandleMoviniment();
- 
     }
 
     private void HandleMoviniment()
     {
-        if (Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetKey(KeyCode.LeftShift))
             _currentSpeed = speedRun;
         else
             _currentSpeed = speed;
@@ -34,12 +47,10 @@ public class Player : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
         }
 
@@ -60,6 +71,39 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidbody.velocity = Vector2.up * forceJump;
+            myRigidbody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidbody.transform);
+
+            HandleScaleJump();
+            
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       // if (collision.transform)
+        {
+         // myRigidbody.velocity = Vector2.down * forcedown;
+          //myRigidbody.transform.localScale = Vector2.one;
+
+          //HandleScaleDown();
+        }
+
+    }
+
+    private void HandleScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+    }
+
+    private void HandleScaleDown()
+    {
+        myRigidbody.transform.DOScaleY(impactScaleY, animationDuration).SetLoops(2, LoopType.Yoyo);
+        myRigidbody.transform.DOScaleX(impactScaleX, animationDuration).SetLoops(2, LoopType.Yoyo);
+
+    }
+
 }
